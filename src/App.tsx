79643676973b596db27a6466f701555603b51850
krautz01 <bekturnurlanov01@gmail.com>
2022-10-29 15:08:17 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App: React.FunctionComponent = () =>  {
+  return (
+    <div className="App">
+      Hello TS
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar: React.FunctionComponent = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
